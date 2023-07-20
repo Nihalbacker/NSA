360bff620875288ceb0892edcb84cfa17c778ec4
Nihal Backer <nihalbackerak@gmail.com>
2023-07-20 10:51:44 +0530

    Add files via upload

---
+++ 11Extract/1.sh
@@ -0,0 +1,12 @@
+linux is a open source operating system
+Linux has been around since the mid-1990s and has since
+reached a user-base that spans the globe.
+ Linux is actually everywhere: It’s in your phones, your thermostats
+in your cars, refrigerators, Roku devices, and televisions.
+linux also runs most of the Internet, all of the world’s top 500 supercomputers
+and the world’s stock exchanges
+ Linux is one of the most reliable, secure and worry-free operating systems available.
+Linux has been around since the mid-1990s and has since
+reached a user-base that spans the globe.
+Linux is actually everywhere: It’s in your phones, your thermostats
+in your cars, refrigerators, Roku devices, and televisions.
